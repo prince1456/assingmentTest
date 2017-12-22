c0fbcf5857d015bc014298e71f5ee4282dca8e50
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-12-22 01:07:55 -0800

    add axios and fetching data from api

---
+++ App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import axios from 'axios'
+import {
+  CachedImage,
+  ImageCacheProvider
+} from 'react-native-cached-image';
+
+
+export default class App extends Component<{}> {
+  state ={
+    data: []
+  }
+  componentDidMount() {
+    axios.get('http://jsonplaceholder.typicode.com/photos')
+    .then(res => {
+         this.setState({data: res.data})
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
